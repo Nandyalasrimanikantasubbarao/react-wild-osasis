9792cee853e23b37ccb7baa2bea015d8d1b455e1
Nandyala Sri Manikanta Subbarao <22h45a6105@bvcits.edu.in>
2025-01-30 08:51:30 +0530

    added

--- src/services/apiCanins.js
+++ src/services/apiCanins.js
@@ -16,3 +16,15 @@ export async function deleteCabin(id) {
     throw new Error("Cabins could not be deleted");
   }
 }
+export async function createCabin(newCabin) {
+  const { data, error } = await supabase
+    .from("cabins")
+    // cabin data get from Form are equal to supabase table data.that why we can directly insert the new cabin
+    .insert([newCabin])
+    .select();
+  if (error) {
+    console.error(error);
+    throw new Error("Cabins could not be deleted");
+  }
+  return data;
+}
