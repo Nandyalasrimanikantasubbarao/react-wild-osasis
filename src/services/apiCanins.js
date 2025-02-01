76de93af32a8b6ecee7d02f626390e90f3585435
Nandyala Sri Manikanta Subbarao <22h45a6105@bvcits.edu.in>
2025-02-01 14:39:19 +0530

    added setting functionality

--- src/services/apiCanins.js
+++ src/services/apiCanins.js
@@ -49,6 +49,8 @@ export async function createEditCabin(newCabin, id) {
     throw new Error("Cabins could not be deleted");
   }
   // upload image
+  if (hasImagePath) return data;
+
   const { error: storageError } = await supabase.storage
     .from("cabin-images")
     .upload(imageName, newCabin.image);
